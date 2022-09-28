63e8ed279d2d537905f9032538c52a5d74c1f006
Davide <me@davegabe.it>
2022-09-28 13:22:27 +0200

    Test all conversions from the paper

--- main.py
+++ main.py
@@ -54,52 +54,56 @@ def main():
     print("Model path:", model_path)
 
     # TRAINING setup
-    source = "VCC2SM3"
-    target = "VCC2TF1"
     epochs_per_save = 300  # Every n epochs, save the model
     num_epochs = 1800  # Max epochs to train
+    conversions_to_test = [ # (source, target) permutations of VCC2SF3 (SF), VCC2SM3 (SM), VCC2TF1 (TF) and VCC2TM1 (TM)
+        #("VCC2SM3", "VCC2TF1"),
+        ("VCC2SM3", "VCC2TM1"),
+        ("VCC2SF3", "VCC2TF1"),
+        ("VCC2SF3", "VCC2TM1"),        
+    ]
 
     # Download the preprocessed dataset and save into dataset_path
-    # download_preprocessed_dataset(
-    #     f"{URL}/{zipfile}", dataset_path)
+    download_preprocessed_dataset(f"{URL}/{zipfile}", dataset_path)
 
-    # Start training process
-    model = f"{source}_to_{target}"
-    training_args = [
-        "--name", model,
-        "--save_dir", model_path,
-        "--preprocessed_data_dir", f"{dataset_path}/vcc2018_training",
-        "--speaker_A_id", source,
-        "--speaker_B_id", target,
-        "--epochs_per_save", f"{epochs_per_save}",
-        "--epochs_per_plot", f"{epochs_per_save}",
-        "--num_epochs", f"{num_epochs}",
-        "--decay_after", "1e4",
-        "--sample_rate", "22050",
-        "--num_frames", "64",
-        "--max_mask_len", "25",
-        "--batch_size", "1",
-        "--continue_train"
-    ]
-    parser_train = CycleGANTrainArgParser()
-    args_train = parser_train.parse_args(training_args)
-    cycleGAN = MaskCycleGANVCTraining(args_train)
-    cycleGAN.train()
+    for source, target in conversions_to_test:
+        # Start training process
+        model = f"{source}_to_{target}"
+        training_args = [
+            "--name", model,
+            "--save_dir", model_path,
+            "--preprocessed_data_dir", f"{dataset_path}/vcc2018_training",
+            "--speaker_A_id", source,
+            "--speaker_B_id", target,
+            "--epochs_per_save", f"{epochs_per_save}",
+            "--epochs_per_plot", f"{epochs_per_save}",
+            "--num_epochs", f"{num_epochs}",
+            "--decay_after", "1e4",
+            "--sample_rate", "22050",
+            "--num_frames", "64",
+            "--max_mask_len", "25",
+            "--batch_size", "1",
+            "--continue_train"
+        ]
+        parser_train = CycleGANTrainArgParser()
+        args_train = parser_train.parse_args(training_args)
+        cycleGAN = MaskCycleGANVCTraining(args_train)
+        cycleGAN.train()
 
-    # Start evaluation process
-    evaluation_args = [
-        "--name", model,
-        "--save_dir", model_path,
-        "--preprocessed_data_dir", f"{dataset_path}/vcc2018_evaluation",
-        "--speaker_A_id", source,
-        "--speaker_B_id", target,
-        "--ckpt_dir", f"{model_path}/{model}/ckpts",
-        "--model_name", "generator_A2B"
-    ]
-    parser_test = CycleGANTestArgParser()
-    args_test = parser_test.parse_args(evaluation_args)
-    tester = MaskCycleGANVCTesting(args_test)
-    tester.test()
+        # Start evaluation process
+        evaluation_args = [
+            "--name", model,
+            "--save_dir", model_path,
+            "--preprocessed_data_dir", f"{dataset_path}/vcc2018_evaluation",
+            "--speaker_A_id", source,
+            "--speaker_B_id", target,
+            "--ckpt_dir", f"{model_path}/{model}/ckpts",
+            "--model_name", "generator_A2B"
+        ]
+        parser_test = CycleGANTestArgParser()
+        args_test = parser_test.parse_args(evaluation_args)
+        tester = MaskCycleGANVCTesting(args_test)
+        tester.test()
 
 
 if __name__ == "__main__":
